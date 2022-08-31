8f26b2d53e727a5b387e8bb8323dadd7cf17af82
kcaldon <kevincaldon@beachhousecenter.com>
2022-08-31 08:24:09 -0400

    Initial commit

---
+++ sql_con.py
@@ -0,0 +1,35 @@
+import psycopg2
+
+Database = '_'
+USER = '_'
+PASSWORD = '_'
+HOST = '_'
+PORT = '_'
+
+conn = psycopg2.connect(database=Database, user=USER, password=PASSWORD, host=HOST, port=PORT)
+
+conn.autocommit = True
+cursor = conn.cursor()
+
+sql1 = "DELETE FROM census_info_beachhouse"
+
+cursor.execute(sql1)
+
+sql2 = '''COPY census_info_beachhouse(instance_id, patient_record_key, patient_master_key,
+ first_name, last_name, mr, admission_date, program_name, length_of_stay, current_ur_loc,
+  current_clinical_loc, insurance_company, age, sex, ethnicity, diagcode_list,
+   diagcodename_list, primarycareteam_primarytherapist, statuses, iscensus,
+    paymentmethod)
+FROM 'C:/SQL_CSV/census_info_beachhouse.csv'
+DELIMITER ','
+CSV HEADER;'''
+
+cursor.execute(sql2)
+
+"""sql3 = '''select * from census_info_beachhouse;'''
+cursor.execute(sql3)
+for i in cursor.fetchall():
+    print(i)"""
+
+conn.commit()
+conn.close()
